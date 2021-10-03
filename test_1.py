1c6daf031c81f996c10aeb11cf4108e7b81fb383
zurirma <rodriguezzurirma@gmail.com>
2021-10-03 12:35:04 -0300

    setup method setted

--- test_1.py
+++ test_1.py
@@ -4,28 +4,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
+
 class Test_Parking:
 
-    def test_1(self):
-        driver = webdriver.Chrome(executable_path='/Users/zury/PycharmProjects/challenge/drivers/chromedriver')
+    def setup_class(self):
+        self.driver = webdriver.Chrome(executable_path='/Users/zury/PycharmProjects/challenge/drivers/chromedriver')
+        self.driver.implicitly_wait(10)
+        self.driver.get('http://www.shino.de/parkcalc/')
+        self.driver.maximize_window()
 
-        driver.implicitly_wait(10)
-        driver.maximize_window()
-
-        driver.get('http://www.shino.de/parkcalc/')
-        opciones = driver.find_element_by_id('ParkingLot')
+    def test_verify_dropdown(self):
+        opciones = self.driver.find_element_by_id('ParkingLot')
         seleccionar = Select(opciones)
         seleccionar.select_by_value('Short')
         assert opciones.get_attribute('name') == 'ParkingLot'
-
-        driver.close()
-        driver.quit()
+        self.driver.close()
+        self.driver.quit()
         print('Test completed')
-
-
-
-
-
-
-
-
