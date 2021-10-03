4cc4ecc01069049be70c0f2eabf2c0e98ab245c7
zurirma <rodriguezzurirma@gmail.com>
2021-10-03 13:55:13 -0300

    first parking cost test

--- test_1.py
+++ test_1.py
@@ -1,4 +1,5 @@
 import select
+import time
 
 from _pytest.fixtures import fixture
 from selenium import webdriver
@@ -20,6 +21,26 @@ class Test_Parking:
         assert title == 'PARKING COST CALCULATOR'
         assert note == 'A Lost Ticket Fee of $10.00 will be assessed when the original parking stub cannot be produced' \
                        ' when exiting the parking facilities (does not apply to Valet Parking).'
+
+
+    def test_valet_parking_cost_in_5_hours(self):
+        opciones = self.driver.find_element_by_id('ParkingLot')
+        seleccionar = Select(opciones)
+        seleccionar.select_by_value('Valet')
+        self.driver.find_element_by_id('StartingDate').clear()
+        self.driver.find_element_by_id('StartingDate').send_keys('10/02/2021')
+        self.driver.find_element_by_id('StartingTime').clear()
+        self.driver.find_element_by_id('StartingTime').send_keys('1:00')
+        self.driver.find_element_by_id('LeavingDate').clear()
+        self.driver.find_element_by_id('LeavingDate').send_keys('10/02/2021')
+        self.driver.find_element_by_id('LeavingTime').clear()
+        self.driver.find_element_by_id('LeavingTime').send_keys('6:00')
+        time.sleep(5)
+        self.driver.find_element_by_name('Submit').click()
+        estimated_parking_costs = self.driver.find_element_by_xpath('/html/body/form/table/tbody/tr[4]/td[2]/span[1]').text
+        estimated_time = self.driver.find_element_by_xpath('/html/body/form/table/tbody/tr[4]/td[2]/span[2]').text
+        assert estimated_parking_costs == '$ 12.00'
+        assert estimated_time == '        (0 Days, 5 Hours, 0 Minutes)'
 
     def test_verify_dropdown(self):
         opciones = self.driver.find_element_by_id('ParkingLot')
